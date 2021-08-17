8ef273187ca549d9b30ebf5367d49ec413e9095a
blackrow67 <dogukanbey42@gmail.com>
2021-08-17 09:50:45 +0000

    m1882: DotOS-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_m1882.mk \
+    $(LOCAL_DIR)/dot_m1882.mk \
     $(LOCAL_DIR)/aosp_m1892.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_m1882-userdebug \
+    dot_m1882-userdebug \
     aosp_m1892-userdebug
