b2b2641ea2fa93fe95ba1272c33313b01bf1c607
blackrow67 <dogukanbey42@gmail.com>
2021-08-17 09:48:28 +0000

    Revert "m1882: SparkOS"
    This reverts commit ff14921b12cba734bffa8fa95b2c97c4dfe2668e.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/spark_m1882.mk \
+    $(LOCAL_DIR)/aosp_m1882.mk \
     $(LOCAL_DIR)/aosp_m1892.mk
 
 COMMON_LUNCH_CHOICES := \
-    spark_m1882-userdebug \
+    aosp_m1882-userdebug \
     aosp_m1892-userdebug
